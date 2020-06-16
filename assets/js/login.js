20029d590583bc7db69e90df265810219e94ae08
Jue-Viole-Grace <fancy199800@gmail.com>
2020-06-16 16:33:12 +0800

    ver 5.2

--- assets/js/login.js
+++ assets/js/login.js
@@ -2,14 +2,24 @@
 $(function(){
     //获取表单对象
     var form = layui.form;
+    //基于layui自定义表单验证规则
+    form.verify({
+        //用户名必须是6-10位字符
+        uname: [/^[\S]{6,10}$/,'用户名必须是6-10位字符'],
+        //密码必须是6位数字
+        pwd: function(value){
+            var reg = /^\d{6}$/;
+            if(!reg.test(value)){
+                return '密码必须是6位数字';
+            };
+        }
+    });
     //表单提交事件(点击登录button)
     $('.layui-form').submit(function(e){
         //阻止默认事件
         e.preventDefault();
         //获取用户名和密码
         var formData = $(this).serialize();
-        //借助layui提供的方法进行表单验证
-        form.verify();
         //发送ajax请求验证用户名和密码
         $.ajax({
             url: 'http://ajax.frontend.itheima.net/api/login',
