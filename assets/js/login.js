20db7c6ea2b38de30dbd1f4c4b50b704f2f8dba3
Jue-Viole-Grace <fancy199800@gmail.com>
2020-06-17 11:01:18 +0800

    ver 5.5

--- assets/js/login.js
+++ assets/js/login.js
@@ -53,13 +53,13 @@ $(function(){
         var formData = $(this).serialize();
         //发送ajax请求验证用户名和密码
         $.ajax({
-            url: 'http://ajax.frontend.itheima.net/api/register',
+            url: 'http://ajax.frontend.itheima.net/api/reguser',
             type: 'post',
             data: formData,
             success: function(backData){
                 //登录成功后跳转到主页
                 if(backData.status == 0){
-                    
+                    $('.login').click();
                 };
             }
         });
