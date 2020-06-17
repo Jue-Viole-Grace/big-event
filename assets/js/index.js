06a3cf613d4c0005b214e12102cca6e083770de9
Jue-Viole-Grace <fancy199800@gmail.com>
2020-06-17 15:50:51 +0800

    ver 6.1

--- assets/js/index.js
+++ assets/js/index.js
@@ -4,4 +4,34 @@ $(function(){
     if(!myToken){
         location.href = './login.html';
     };
+
+    //获取用户信息
+    function loadUserInfo (){
+        $.ajax({
+            url: 'http://ajax.frontend.itheima.net/my/userinfo',
+            type: 'get',
+            //请求头
+            headers: {
+                Authorization: myToken
+            },
+            success: function(backData){
+                if(backData.status == 0){
+                    var info = backData.data;
+                    //用户信息填充
+                    $('.welcome #username').html(info.username);
+                    $('#nav-username').html(info.username);
+                    //填充头像
+                    // info.user_pic = 'http://t.cn/RCzsdCq';
+                    if(info.user_pic){
+                        //如果头像存在，则显示一张图片
+                        $('.welcome').prepend('<img src="'+ info.user_pic +'">');
+                        $('.welcome div').remove();
+                    };
+                };
+            }
+        });
+    };
+    loadUserInfo();
+
+    //退出功能
 });
