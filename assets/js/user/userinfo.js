62b04c5aef79a3463040011c31afc1bd0e894a23
Jue-Viole-Grace <fancy199800@gmail.com>
2020-06-19 11:51:36 +0800

    1151

--- assets/js/user/userinfo.js
+++ assets/js/user/userinfo.js
@@ -1,13 +1,16 @@
 $(function(){
+    var form = layui.form;
     //调用接口加载用户信息
     function loadUserInfo (){
         $.ajax({
             url: 'my/userinfo',
             type: 'get',
             success: function(res){
-                $('.layui-form input[name=username]').val(res.data.username);
-                $('.layui-form input[name=nickname]').val(res.data.nickname);
-                $('.layui-form input[name=email]').val(res.data.email);
+                // $('.layui-form input[name=username]').val(res.data.username);
+                // $('.layui-form input[name=nickname]').val(res.data.nickname);
+                // $('.layui-form input[name=email]').val(res.data.email);
+                //基于layui填充表单
+                form.val('userInfo',res.data);
             }
         });
     };
